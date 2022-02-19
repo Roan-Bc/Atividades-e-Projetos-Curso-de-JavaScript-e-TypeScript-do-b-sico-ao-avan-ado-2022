0b7e9fbef56d4fe554172ee0a9132a46ff264d42
Roan Bittencourt de Carvalho <56409084+Roan-Bc@users.noreply.github.com>
2022-02-18 22:43:51 -0300

    Add files via upload

---
+++ Atividades/Atividade 14/assets/js/main.js
@@ -0,0 +1,54 @@
+/*
+
+Atividade 14
+
+Desenvolva um timer em js que tenha a função de iniciar, pausar e zerar, e quando pausar o timer, 
+o número deve ficar em vermelho.
+
+
+*/
+
+
+function relogio() {
+  function criaHoraDosSegundos(segundos) {
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString('pt-BR', {
+      hour12: false,
+      timeZone: 'UTC'
+    });
+  }
+
+  const relogio = document.querySelector('.relogio');
+  let segundos = 0;
+  let timer;
+
+  function iniciaRelogio() {
+    timer = setInterval(function () {
+      segundos++;
+      relogio.innerHTML = criaHoraDosSegundos(segundos);
+    }, 1000);
+  }
+
+  document.addEventListener('click', function (e) {
+    const el = e.target;
+
+    if (el.classList.contains('zerar')) {
+      clearInterval(timer);
+      relogio.innerHTML = '00:00:00';
+      relogio.classList.remove('pausado');
+      segundos = 0;
+    }
+
+    if (el.classList.contains('iniciar')) {
+      relogio.classList.remove('pausado');
+      clearInterval(timer);
+      iniciaRelogio();
+    }
+
+    if (el.classList.contains('pausar')) {
+      clearInterval(timer);
+      relogio.classList.add('pausado');
+    }
+  });
+}
+relogio();
