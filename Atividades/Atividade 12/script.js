2902df8cc88decd15934d8ad5e9bf4eecdaf794b
Roan Bittencourt de Carvalho <56409084+Roan-Bc@users.noreply.github.com>
2022-02-18 21:41:46 -0300

    Add files via upload

---
+++ Atividades/Atividade 12/script.js
@@ -0,0 +1,13 @@
+/* 
+
+12° Atividade
+
+Escreva uma função chamada ePaisagem que recebe dois argumentos, 
+largura e altura de uma imagem (number). E Retorne true se a imagem estiver no modo
+paisagem.
+
+*/
+
+const ePaisagem = (largura, altura) => largura > altura;
+
+console.log(ePaisagem(1200, 1000));
